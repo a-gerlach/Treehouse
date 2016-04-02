70b381ab3eaffac1b74a1fb6bb9a4dc2e04499ee
addiger <adrian.gerlach89@gmail.com>
2016-04-02 08:51:03 +0000

    Test Commit

---
+++ name.rb
@@ -0,0 +1,34 @@
+class Name
+    
+    
+    
+    attr_accessor :title, :first_name, :middle_name, :last_name
+    
+    def initialize(title, first_name, middle_name, last_name)
+        @title = title
+        @first_name = first_name
+        @middle_name = middle_name
+        @last_name = last_name
+    end
+    
+    def full_name
+       @first_name + " " + @middle_name  + " " + @last_name
+    end
+    
+    def full_name_with_title
+        @title + " " + full_name() 
+    end
+    
+    def to_s
+       full_name_with_title 
+    end
+   
+end
+
+
+
+name = Name.new("Mr.", "Adrian", "Calvin", "Gerlach")
+puts name.full_name_with_title
+    
+puts name
+puts name.inspect
